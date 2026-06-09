e3a3de954150b77fef5bcc5a23993b330bac5b36
Abhishek Parmar <abhishek205parmar@gmail.com>
2026-06-09 12:18:24 +0530

    [Abhishek]Add:Day1 all questions

--- core-java-practice/leet-code-codebase/Day1/Palindrome_Number.java
+++ core-java-practice/leet-code-codebase/Day1/Palindrome_Number.java
@@ -1,16 +1,34 @@
-class Solution {
-  public boolean isPalindrome(int x) {
-    if (x < 0)
-      return false;
+import java.util.Scanner;
 
-    long reversed = 0;
-    int y = x;
+public class Palindrome_Number {
 
-    while (y > 0) {
-      reversed = reversed * 10 + y % 10;
-      y /= 10;
+    public static boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        long reversed = 0;
+        int y = x;
+
+        while (y > 0) {
+            reversed = reversed * 10 + y % 10;
+            y /= 10;
+        }
+
+        return reversed == x;
     }
 
-    return reversed == x;
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a Palindrome.");
+        } else {
+            System.out.println(num + " is not a Palindrome.");
+        }
+
+        sc.close();
+    }
 }
--- core-java-practice/leet-code-codebase/Day1/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
+++ core-java-practice/leet-code-codebase/Day1/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
@@ -1,13 +1,31 @@
-class Solution {
-  public int subtractProductAndSum(int n) {
-    int prod = 1;
-    int summ = 0;
+import java.util.Scanner;
 
-    for (; n > 0; n /= 10) {
-      prod *= n % 10;
-      summ += n % 10;
+public class Subtract_the_Product_and_Sum_of_Digits_of_an_Integer {
+
+    public static int subtractProductAndSum(int n) {
+        int prod = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            prod *= digit;
+            sum += digit;
+            n /= 10;
+        }
+
+        return prod - sum;
     }
 
-    return prod - summ;
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int result = subtractProductAndSum(n);
+
+        System.out.println("Result = " + result);
+
+        sc.close();
+    }
 }
